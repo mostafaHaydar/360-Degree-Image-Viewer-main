bc362305b0f543aaec717136efa4ca4522576c2c
mostafa haydar <88322239+mostafaHaydar@users.noreply.github.com>
2024-08-02 15:26:22 +0100

    i made this new repo just for the UI of the 360 viewer

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function viewer(image, container, options = {}) {
+  const panoramaImage = new PANOLENS.ImagePanorama(`${image}`);
+  const imageContainer = document.querySelector(`.${container}`);
+
+  const defaultOptions = {
+    autoRotate: true,
+    autoRotateSpeed: 0.3,
+    controlBar: true,
+    pointerLock: false,
+    backgroundColor: 0x000000,
+    renderStats: false,
+  };
+
+  const viewerOptions = { ...defaultOptions, ...options };
+
+  const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    autoRotate: viewerOptions.autoRotate,
+    autoRotateSpeed: viewerOptions.autoRotateSpeed,
+    controlBar: viewerOptions.controlBar,
+    pointerLock: viewerOptions.pointerLock,
+    backgroundColor: viewerOptions.backgroundColor,
+    renderStats: viewerOptions.renderStats,
+  });
+
+  viewer.add(panoramaImage);
+
+  if (options.pause) {
+    viewer.pause();
+  }
+  if (options.resume) {
+    viewer.resume();
+  }
+}
